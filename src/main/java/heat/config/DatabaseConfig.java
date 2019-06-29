5ac55df39dc86b473885a19ad74ba9af7526acb8
Igor Doroshev <igdor9399@gmail.com>
2019-06-30 00:24:15 +0300

    Fixed database connection.

--- src/main/java/heat/config/DatabaseConfig.java
+++ src/main/java/heat/config/DatabaseConfig.java
@@ -1,22 +1,25 @@
 package heat.config;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public DataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        HikariDataSource basicDataSource = new HikariDataSource();
+        basicDataSource.setJdbcUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
