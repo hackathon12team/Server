a5b1804b2dc7932b48b13c5abcca1f76ba59cf94
Igor Doroshev <igdor9399@gmail.com>
2019-06-29 21:06:54 +0300

    Added test request handler.

--- src/main/java/heat/BackendApplication.java
+++ src/main/java/heat/BackendApplication.java
@@ -2,9 +2,18 @@ package heat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class BackendApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
